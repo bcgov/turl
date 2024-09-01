cc2e4a701941380dca462dfa4bfec1e832b471f9
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2024-09-01 16:23:22 -0700

    prisma fix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,25 +1,12 @@
 import "dotenv/config";
-import { Logger, MiddlewareConsumer, Module, RequestMethod } from "@nestjs/common";
+import { MiddlewareConsumer, Module, RequestMethod } from "@nestjs/common";
 import { HTTPLoggerMiddleware } from "./middleware/req.res.logger";
-import { loggingMiddleware } from "nestjs-prisma";
 import { ConfigModule } from "@nestjs/config";
 import { AppService } from "./app.service";
 import { AppController } from "./app.controller";
 import { MetricsController } from "./metrics.controller";
 import { TerminusModule } from '@nestjs/terminus';
 
-function getMiddlewares() {
-  if (process.env.PRISMA_LOGGING) {
-    return [
-      // configure your prisma middleware
-      loggingMiddleware({
-        logger: new Logger("PrismaMiddleware"),
-        logLevel: "debug"
-      })
-    ];
-  }
-  return [];
-}
 
 @Module({
   imports: [
