919414ec6d00b98d47c5813adc2a1c7ca3e6ca8a
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2024-09-01 15:51:11 -0700

    remove prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,22 +1,13 @@
 import "dotenv/config";
 import { Logger, MiddlewareConsumer, Module, RequestMethod } from "@nestjs/common";
 import { HTTPLoggerMiddleware } from "./middleware/req.res.logger";
-import { loggingMiddleware, PrismaModule } from "nestjs-prisma";
+import { loggingMiddleware } from "nestjs-prisma";
 import { ConfigModule } from "@nestjs/config";
-import { UsersModule } from "./users/users.module";
 import { AppService } from "./app.service";
 import { AppController } from "./app.controller";
 import { MetricsController } from "./metrics.controller";
 import { TerminusModule } from '@nestjs/terminus';
-import { HealthController } from "./health.controller";
 
-const DB_HOST = process.env.POSTGRES_HOST || "localhost";
-const DB_USER = process.env.POSTGRES_USER || "postgres";
-const DB_PWD = encodeURIComponent(process.env.POSTGRES_PASSWORD || "default"); // this needs to be encoded, if the password contains special characters it will break connection string.
-const DB_PORT = process.env.POSTGRES_PORT || 5432;
-const DB_NAME = process.env.POSTGRES_DATABASE || "postgres";
-const DB_SCHEMA = process.env.DB_SCHEMA || "users";
-const dataSourceURL = `postgresql://${DB_USER}:${DB_PWD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?schema=${DB_SCHEMA}&connection_limit=5`;
 function getMiddlewares() {
   if (process.env.PRISMA_LOGGING) {
     return [
@@ -34,20 +25,8 @@ function getMiddlewares() {
   imports: [
     ConfigModule.forRoot(),
     TerminusModule,
-    PrismaModule.forRoot({
-      isGlobal: true,
-      prismaServiceOptions:{
-        prismaOptions:{
-          log: ["error", "warn"],
-          errorFormat: "pretty",
-          datasourceUrl: `postgresql://${DB_USER}:${DB_PWD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?schema=${DB_SCHEMA}&connection_limit=5`,
-        },
-        middlewares: getMiddlewares(),
-      },
-    }),
-    UsersModule
   ],
-  controllers: [AppController,MetricsController, HealthController],
+  controllers: [AppController,MetricsController],
   providers: [AppService]
 })
 export class AppModule { // let's add a middleware on all routes
