adb5cd62e009c8a4c4ffe51f58614e68ed78148f
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2024-09-02 15:55:51 -0700

    add masterset name

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from "@nestjs/common";
+import {Inject, Injectable, OnModuleDestroy} from "@nestjs/common";
+import {Redis} from "ioredis";
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return "Hello Backend!";
+export class AppService implements OnModuleDestroy{
+  constructor(@Inject("REDIS") private readonly redis: Redis) {
+  }
+  onModuleDestroy(): any {
+    this.redis.disconnect();
+  }
+
+  async getHello(): Promise<string> {
+    return this.redis.ping();
   }
 }
