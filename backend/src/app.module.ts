49623018ed025874b8f139cdfff5bee9683edf43
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2024-09-02 10:43:55 -0700

    reduce size

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,23 +1,25 @@
 import "dotenv/config";
-import { MiddlewareConsumer, Module, RequestMethod } from "@nestjs/common";
-import { HTTPLoggerMiddleware } from "./middleware/req.res.logger";
-import { ConfigModule } from "@nestjs/config";
-import { AppService } from "./app.service";
-import { AppController } from "./app.controller";
-import { MetricsController } from "./metrics.controller";
-import { TerminusModule } from '@nestjs/terminus';
-
+import {MiddlewareConsumer, Module, RequestMethod} from "@nestjs/common";
+import {HTTPLoggerMiddleware} from "./middleware/req.res.logger";
+import {ConfigModule} from "@nestjs/config";
+import {AppService} from "./app.service";
+import {AppController} from "./app.controller";
+import {MetricsController} from "./metrics.controller";
+import {TerminusModule} from '@nestjs/terminus';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TerminusModule,
   ],
-  controllers: [AppController,MetricsController],
+  controllers: [AppController, MetricsController],
   providers: [AppService]
 })
 export class AppModule { // let's add a middleware on all routes
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(HTTPLoggerMiddleware).exclude({ path: 'metrics', method: RequestMethod.ALL }, { path: 'health', method: RequestMethod.ALL }).forRoutes('*');
+    consumer.apply(HTTPLoggerMiddleware).exclude({path: 'metrics', method: RequestMethod.ALL}, {
+      path: 'health',
+      method: RequestMethod.ALL
+    }).forRoutes('*');
   }
 }
